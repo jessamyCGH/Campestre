f652d7472fa731deb38cada4bf720a8b123591fe
Ruben Elizalde <ruben290598@hotmail.com>
2021-03-28 14:48:56 -0600

    Frame Check
    Se agregaron los boton de seleccion

---
+++ WindowsFormsApp3/WindowsFormsApp3/Check.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Check
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button7 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.Image = global::WindowsFormsApp3.Properties.Resources.Clausura;
+            this.button7.Location = new System.Drawing.Point(800, 40);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 111);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Clausura";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(228)))), ((int)(((byte)(229)))));
+            this.button4.Image = global::WindowsFormsApp3.Properties.Resources.Apertura;
+            this.button4.Location = new System.Drawing.Point(98, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 111);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Inaguracion";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::WindowsFormsApp3.Properties.Resources.golf;
+            this.button5.Location = new System.Drawing.Point(566, 40);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 111);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Golf";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::WindowsFormsApp3.Properties.Resources.tennis;
+            this.button6.Location = new System.Drawing.Point(449, 40);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 111);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Tennis";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::WindowsFormsApp3.Properties.Resources.cerveza;
+            this.button3.Location = new System.Drawing.Point(332, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 111);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Cerveza";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::WindowsFormsApp3.Properties.Resources.concierto;
+            this.button2.Location = new System.Drawing.Point(683, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 111);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Concierto";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WindowsFormsApp3.Properties.Resources.alimentos;
+            this.button1.Location = new System.Drawing.Point(215, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 111);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Alimentos";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Check
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1105, 695);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Check";
+            this.Text = "Check";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
