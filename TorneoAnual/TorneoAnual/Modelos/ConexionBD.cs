e519129d4fa2893e59dd8b7fd2f8adb050c7d42a
Jessamy Guzman <jessamy0798@gmail.com>
2021-04-15 00:13:29 -0500

    Xaml
    Proyecto con avances en huella y registro en la base de datos. Frame principal en xaml y huella en ,cs

---
+++ TorneoAnual/TorneoAnual/Modelos/ConexionBD.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorneoAnual.Modelos
+{
+    class ConexionBD
+    {
+        public ConexionBD() { }
+
+        public static int AltaEmpleado(Usuario usuario)
+        {
+            int res = 0;
+
+            try
+            {
+                using (var conn = new SqlConnection("Data Source = localhost; initial catalog = Checador; Integrated Security = True "))
+                {
+                    conn.Open();
+
+                    using (var command = conn.CreateCommand())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "AltaEmpleados";
+                        command.Parameters.AddWithValue("@Nombre", usuario.Nombre);
+                        command.Parameters.AddWithValue("@ApellidoP", usuario.ApellidoP);
+                        command.Parameters.AddWithValue("@ApellidoM", usuario.ApellidoM);
+                        command.Parameters.AddWithValue("@Numero", usuario.Tel);
+                        command.Parameters.AddWithValue("@Foto", usuario.Foto);
+                        command.Parameters.AddWithValue("@Huella", usuario.Huella);
+
+                        SqlParameter param = new SqlParameter("Id", SqlDbType.Int);
+                        param.Value = 0;
+                        param.Direction = ParameterDirection.Output;
+                        command.Parameters.Add(param);
+
+                        res = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al dar de alta al empleado: " + ex.Message, "Error en Alta");
+            }
+
+            return res;
+
+        }
+
+
+    }
+}
+
